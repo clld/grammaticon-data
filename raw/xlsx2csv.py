30ce3383e38266d439d4dc35e33e14202579b915
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-02-21 11:13:45 +0100

    python implementation of the xlsx→csv conversion
    (the libreoffice-calling version did not ensure utf8 encoding)

--- raw/xlsx2csv.py
+++ raw/xlsx2csv.py
@@ -1,13 +1,48 @@
-from subprocess import check_call
+#!/usr/bin/env python3
 
-from clldutils.path import Path
-from clldutils.dsv import UnicodeWriter
+import csv
+from itertools import chain, repeat
+from pathlib import Path
+
+from openpyxl import load_workbook
 
 
-def xlsx2csv(p):
-    check_call(['soffice', '--headless', '--convert-to', 'csv', str(p)])
+def normalise_cell(value):
+    if value is None:
+        return ''
+    else:
+        return str(value).strip()
+
+
+def pad_list(ls, width):
+    if len(ls) == width:
+        return ls
+    elif len(ls) < width:
+        return list(chain(ls, repeat('', width - len(ls))))
+    else:
+        raise ValueError(f'too long: {ls}')
+
+
+def xlsx2csv(path):
+    wb = load_workbook(filename=str(path), read_only=True, data_only=True)
+    worksheets = wb.worksheets
+    assert len(worksheets) == 1, f'{path}: not exactly 1 worksheet'
+    sheet = worksheets[0]
+    rows = [
+        row_norm
+        for row in sheet.iter_rows()
+        if any(row_norm := [normalise_cell(cell.value) for cell in row])]
+
+    table_width = max(len(row) for row in rows)
+    with open(path.with_suffix('.csv'), 'w', encoding='utf-8') as f:
+        wtr = csv.writer(f)
+        wtr.writerows(pad_list(row, table_width) for row in rows)
+
+
+def main():
+    for p in Path('.').glob('*.xlsx'):
+        xlsx2csv(p)
 
 
 if __name__ == '__main__':
-    for p in Path('.').glob('*.xlsx'):
-        xlsx2csv(p)
+    main()
